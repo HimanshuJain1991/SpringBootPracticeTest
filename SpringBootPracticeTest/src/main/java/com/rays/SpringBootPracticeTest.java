8d99a7cfb9aec7b54fe42ce5b72eb4a9aad00fe1
HimanshuJain1991 <himanshujain119@yahoo.com>
2024-04-27 16:42:18 +0530

    done

---
+++ SpringBootPracticeTest/src/main/java/com/rays/SpringBootPracticeTest.java
@@ -0,0 +1,11 @@
+package com.rays;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeTest {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeTest.class, args);
+	}
+
+}
